3c71e56fd4a229cc6abf827e486be2af1a5c6197
macid <macidori98@gmail.com>
2019-11-13 14:13:06 +0200

    traineemusclegroupfragment created

---
+++ app/src/main/java/com/example/fitnessapplication/TraineeMuscleGroupsFragment.java
@@ -0,0 +1,56 @@
+package com.example.fitnessapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TraineeMuscleGroupsFragment extends Fragment {
+
+    public final static String TAG = TraineeMuscleGroupsFragment.class.getSimpleName();
+
+    private View view;
+    private LinearLayout linearLayout_trainee_muscle_group;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_trainee_muscle_group, container, false);
+        linearLayout_trainee_muscle_group = view.findViewById(R.id.linearLayout_trainee_muscle_groups);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        addButtons();
+    }
+
+    private void addButtons(){
+        final List<String> buttonString = Arrays.asList(getResources().getStringArray(R.array.trainee_muscle_groups));
+        for (int i = 0; i < buttonString.size(); ++i){
+            Button btn = new Button(getContext());
+            btn.setText(buttonString.get(i));
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //go to fragment trainee muscle group exercises
+                    //passing somehow which muscle group should be shown
+                }
+            });
+            linearLayout_trainee_muscle_group.addView(btn);
+        }
+    }
+}
