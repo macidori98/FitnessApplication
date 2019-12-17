0bb3a9f4c0c2e7997235eb6661a1a9a405eb5400
MozesVidami <mozeskec@gmail.com>
2019-12-17 10:09:20 +0200

    Trainer my content done Trainer upload with hardcoded video

--- app/src/main/java/com/example/fitnessapplication/fragment/trainer/TrainerMyContentFragment.java
+++ app/src/main/java/com/example/fitnessapplication/fragment/trainer/TrainerMyContentFragment.java
@@ -1,6 +1,7 @@
 package com.example.fitnessapplication.fragment.trainer;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,19 +9,46 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fitnessapplication.R;
+import com.example.fitnessapplication.adapter.TrainerMyContentAdapter;
 import com.example.fitnessapplication.fragment.trainee.TraineeTrainersFragment;
+import com.example.fitnessapplication.model.ExerciseVideo;
+import com.example.fitnessapplication.utils.Constant;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrainerMyContentFragment extends Fragment {
 
     public final static String TAG = TraineeTrainersFragment.class.getSimpleName();
     private View view;
+    private List<ExerciseVideo> videoList;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
+    private RecyclerView recyclerView_trainer_my_content;
+    private TrainerMyContentAdapter mAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_trainer_my_content, container, false);
+
+
+        videoList = new ArrayList<>();
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference(Constant.EXERCISE_VIDEO);
+        recyclerView_trainer_my_content = view.findViewById(R.id.recyclerView_trainer_my_content);
+        recyclerView_trainer_my_content.setLayoutManager(new LinearLayoutManager(getContext()));
+        getData();
+
         initializeViewElements(view);
         return view;
     }
@@ -29,6 +57,33 @@ public class TrainerMyContentFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+    }
+
+    private void getData() {
+        mRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String trainer_id = snapshot.child(Constant.TRAINER_ID).getValue().toString();
+                    if (trainer_id.toLowerCase().equals(Constant.CURRENT_USER.getId().toLowerCase())) {
+                        String id = snapshot.child(Constant.ID).getValue().toString();
+                        String url = snapshot.child(Constant.URL).getValue().toString();
+                        String title = snapshot.child(Constant.TITLE).getValue().toString();
+                        String description = snapshot.child(Constant.DESCRIPTION).getValue().toString();
+                        String musclegroup = snapshot.child(Constant.MUSCLE_GROUP).getValue().toString();
+                        ExerciseVideo exerciseVideo = new ExerciseVideo(id, trainer_id, url, title, musclegroup, description);
+                        videoList.add(exerciseVideo);
+                    }
+                }
+                mAdapter = new TrainerMyContentAdapter(videoList, getActivity());
+                recyclerView_trainer_my_content.setAdapter(mAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //Log.e("TraineMuscleGroupError","TraineMuscleGroupError");
+            }
+        });
     }
 
     private void initializeViewElements(View view) {
--- app/src/main/java/com/example/fitnessapplication/fragment/trainer/TrainerUploadFragment.java
+++ app/src/main/java/com/example/fitnessapplication/fragment/trainer/TrainerUploadFragment.java
@@ -4,18 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.fitnessapplication.R;
+import com.example.fitnessapplication.model.ExerciseVideo;
+import com.example.fitnessapplication.utils.Constant;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class TrainerUploadFragment extends Fragment {
 
     public final static String TAG = TrainerUploadFragment.class.getSimpleName();
 
     private View view;
+    private EditText et_upload_title,et_upload_description;
+    private Button btn_trainer_upload_done;
+    private Spinner sp_upload_musclegroup;
+    private ArrayAdapter<CharSequence> adapter;
 
     @Nullable
     @Override
@@ -28,9 +41,43 @@ public class TrainerUploadFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        btn_trainer_upload_done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickUpload();
+            }
+        });
     }
 
     private void initializeViewElements(View view) {
+        et_upload_title = view.findViewById(R.id.et_trainer_upload_title);
+        et_upload_description = view.findViewById(R.id.et_trainer_upload_description);
+        sp_upload_musclegroup = view.findViewById(R.id.sp_upload_content);
+        btn_trainer_upload_done = view.findViewById(R.id.btn_trainer_upload_upload);
+        setupSpinner(view);
+    }
 
+    private void onClickUpload(){
+        FirebaseDatabase p = FirebaseDatabase.getInstance();
+        DatabaseReference ref = p.getReference(Constant.EXERCISE_VIDEO);
+        String key = ref.push().getKey();
+        String title = et_upload_title.getText().toString();
+        String description = et_upload_description.getText().toString();
+        int idx = (int) sp_upload_musclegroup.getSelectedItemId();
+        String muscleGroupSelected = adapter.getItem(idx).toString();
+        ExerciseVideo exerciseVideo = new ExerciseVideo(key,Constant.CURRENT_USER.getId(),"https://firebasestorage.googleapis.com/v0/b/fitnessapplication2.appspot.com/o/video-1576262209.mp4?alt=media&token=c9e0521d-6e34-4166-a690-967b8da344ca",title,muscleGroupSelected,description);
+        ref.child(key).setValue(exerciseVideo);
+        Toast.makeText(getContext(), R.string.upload_successful, Toast.LENGTH_SHORT).show();
+    }
+
+    private void setupSpinner(View view){
+        sp_upload_musclegroup= view.findViewById(R.id.sp_upload_content);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.trainee_muscle_groups, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        sp_upload_musclegroup.setAdapter(adapter);
     }
 }
